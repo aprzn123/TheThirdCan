4a0b5c7f8a6b748e80d07b336b28876c5b87f391
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2022-11-18 12:04:44 -0800

    emacs configuration

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((javascript-mode . ((js-indent-level . 2)
+                     (evil-shift-width . 2)
+                     (fill-column . 100) ;; NOTE: 80 or 100?
+                     (lsp . 1))))
