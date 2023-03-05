d02961bc571be57cf2dde9f646bfda8cb8a0236f
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2023-03-05 15:19:29 -0800

    respect skip confirmation setting when skipping for now

--- src/injected/skipForNowButton.js
+++ src/injected/skipForNowButton.js
@@ -13,10 +13,10 @@
   function shouldSkip() {
     const cfgStr = window.sessionStorage.getItem("ttcConfigState");
     const cfg = JSON.parse(cfgStr);
-    if (cfg.skipConfirm) {
+    if (cfg.skipConfirm !== undefined) {
       // check if user enabled skip confirmation. if so, prompt for confirmation
       // if necessary according to the user's settings
-      if ((cfg.skipConfirm === 1 && textbox.value === "") || cfg.skipConfirm === 2) {
+      if ((cfg.skipConfirm === 1 && textbox.value !== "") || cfg.skipConfirm === 2) {
         return confirm("Are you sure you want to skip this question for now?");
       }
     }
@@ -27,6 +27,7 @@
     if (shouldSkip()) {
       // the best way to get a new question would be to reinit the QA
       TC.QA.Answer.init(userID);
+      textbox.value = "";
     }
   };
 }
