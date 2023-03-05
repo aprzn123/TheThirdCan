da934e90ee543dd77b3ef74b0612bd689ee00de2
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2023-03-05 08:10:57 -0800

    change `settings` to `ttcSettings`

--- src/js/thirdcan.js
+++ src/js/thirdcan.js
@@ -1,6 +1,6 @@
-const settings = fetch(browser.runtime.getURL("injected/default_settings.json"))
+const ttcSettings = fetch(browser.runtime.getURL("injected/default_settings.json"))
     .then((response) => response.json())
     .then((settings) => browser.storage.sync.get(settings));
-settings.then((cfg) => {
+ttcSettings.then((cfg) => {
   window.localStorage.setItem("cfg", JSON.stringify(cfg));
 });
