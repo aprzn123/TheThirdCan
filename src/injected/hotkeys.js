459f6a4bb4d66bcc9dcc2ddd94a31e5efbbb4345
Your Name <you@example.com>
2023-03-06 18:13:59 +0200

    change indentation

--- src/injected/hotkeys.js
+++ src/injected/hotkeys.js
@@ -1,19 +1,19 @@
-    let answerButton = document.querySelector("#content_host table tr").childNodes[1].childNodes[1].childNodes[0]; 
-    let saveButton = document.querySelector("#content_host table tr").childNodes[1].childNodes[1].childNodes[1]; 
-    let skipButton = document.querySelector("#content_host table tr").childNodes[1].childNodes[1].childNodes[2]; 
+let answerButton = document.querySelector("#content_host table tr").childNodes[1].childNodes[1].childNodes[0];
+let saveButton = document.querySelector("#content_host table tr").childNodes[1].childNodes[1].childNodes[1];
+let skipButton = document.querySelector("#content_host table tr").childNodes[1].childNodes[1].childNodes[2];
 
-document.addEventListener("keydown", function(event) {
-    if (event.ctrlKey && event.key == "Enter") {
-        answerButton.click();
-    }
+document.addEventListener("keydown", function (event) {
+  if (event.ctrlKey && event.key == "Enter") {
+    answerButton.click();
+  }
 
-    else if (event.ctrlKey && event.key == "s") {
-        event.preventDefault();
-        saveButton.click();
-    }
+  else if (event.ctrlKey && event.key == "s") {
+    event.preventDefault();
+    saveButton.click();
+  }
 
-    else if (event.ctrlKey && event.key == "Backspace") {
-        skipButton.click();
-    }
+  else if (event.ctrlKey && event.key == "Backspace") {
+    skipButton.click();
+  }
 
-  });
+});
