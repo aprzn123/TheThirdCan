67677d2d80bd9fa1fc68f4598fcb4a7362aeff0b
HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>
2023-03-05 16:17:24 -0800

    feat: skip for now button (#3)
    * emacs configuration
    
    * skip for now button
    
    * Merge `cfg-localstorage` da934e9 and `feat-skip-for-now-button` 644f3ec
    
    * move injected script to correct path
    
    * skip for now settings
    
    * untrack .dir-locals.el
    
    * refactor: adapt to changes from cfg-localstorage branch
    
    * respect skip confirmation setting when skipping for now

---
+++ src/injected/skipForNowButton.js
@@ -0,0 +1,33 @@
+{
+  let textbox = document.getElementById("answer_text");
+  let purpleButton = document.querySelector("button.purple");
+  let buttonArea = purpleButton.parentElement;
+  let skipForNowButton = document.createElement("button");
+  skipForNowButton.innerText = "Skip for now";
+  buttonArea.insertBefore(skipForNowButton, purpleButton.nextSibling);
+
+  // hacky way to get user id
+  const attr = document.querySelector("body").getAttribute("onload");
+  const userID = Number(attr.substring(18, attr.length - 1));
+
+  function shouldSkip() {
+    const cfgStr = window.sessionStorage.getItem("ttcConfigState");
+    const cfg = JSON.parse(cfgStr);
+    if (cfg.skipConfirm !== undefined) {
+      // check if user enabled skip confirmation. if so, prompt for confirmation
+      // if necessary according to the user's settings
+      if ((cfg.skipConfirm === 1 && textbox.value !== "") || cfg.skipConfirm === 2) {
+        return confirm("Are you sure you want to skip this question for now?");
+      }
+    }
+    return true;
+  }
+
+  skipForNowButton.onclick = () => {
+    if (shouldSkip()) {
+      // the best way to get a new question would be to reinit the QA
+      TC.QA.Answer.init(userID);
+      textbox.value = "";
+    }
+  };
+}
